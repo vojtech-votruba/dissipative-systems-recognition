30cbac9fdfcc35b2e57fa07c7eb223833b83da78
Vojtěch Votruba <vojtech.votruba@karlin.mff.cuni.cz>
2024-09-07 00:31:34 +0200

    fixed the PICNN implementation, added entropy as a concave function, added minibatching

--- gradient_dynamics/learn_and_compare.py
+++ gradient_dynamics/learn_and_compare.py
@@ -3,10 +3,11 @@
 import os
 import argparse
 import numpy as np
-from sklearn.model_selection import train_test_split
 import torch
 from torch import nn
 from torch import autograd
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import random_split
 from matplotlib import pyplot as plt
 
 # Extracting the data
@@ -16,39 +17,59 @@ if os.path.exists("data/dataset.txt") is False:
 with open("data/dataset.txt", "r", encoding="utf-8") as f:
     data_raw = f.read().strip().split("\n\n")
 
-data = [
-        [[float(value) for value in line.split(',')] for line in mat_str.strip().split('\n')]
-        for mat_str in data_raw
-    ]
-data = np.array(data)
-input_data = data[:,1:-2,:]
-target = data[:,2:-1,:]
-train_data, test_data, train_target, test_target = train_test_split(
-    input_data, target, test_size=0.2, random_state=42)
+class TrajectoryDataset(Dataset):
+        def __init__(self):
+                loaded_trajectories = [
+                        [[float(value) for value in line.split(',')] for line in mat_str.strip().split('\n')]
+                        for mat_str in data_raw
+                    ]
+                loaded_trajectories = np.array(loaded_trajectories)
+                data = loaded_trajectories[:,1:-2,:]
+                target = loaded_trajectories[:,2:-1,:]
 
-DIMENSION = int((len(train_data[0,0,:])-1)/2)
+                global DIMENSION
+                DIMENSION = int((len(data[0,0,:])-1)/2)
+
+                self.data = torch.tensor(data[:,:,1:DIMENSION+1], requires_grad=True)
+                self.target = torch.tensor(target[:,:,1:DIMENSION+1], requires_grad=True)
+
+                """self.data = torch.tensor(data[:,:,1:DIMENSION+1].reshape(
+                    (data[:,:,1:DIMENSION+1].shape[0] * data[:,:,1:DIMENSION+1].shape[1], data[:,:,1:DIMENSION+1].shape[2])), requires_grad=True)
+                
+                self.target = torch.tensor(target[:,:,1:DIMENSION+1].reshape(
+                    (target[:,:,1:DIMENSION+1].shape[0] * target[:,:,1:DIMENSION+1].shape[1], target[:,:,1:DIMENSION+1].shape[2])), requires_grad=True)"""
+                
+                self.n_samples = self.data.shape[0]
+
+        def __getitem__(self, index):
+            return self.data[index], self.target[index]
+
+        def __len__(self):
+            return self.n_samples
+
+trajectories = TrajectoryDataset()
 
 """
-For neural networks that are modeling inertia
+Older implementation
 
 reshaped_train_data = torch.tensor(train_data[:,:,1:].reshape((train_data[:,:,1:].shape[0] * train_data[:,:,1:].shape[1], train_data[:,:,1:].shape[2])))
 reshaped_train_target = torch.tensor(train_target[:,:,1:].reshape((train_target[:,:,1:].shape[0] * train_target[:,:,1:].shape[1], train_target[:,:,1:].shape[2])))
 reshaped_test_data = torch.tensor(test_data[:,:,1:].reshape((test_data[:,:,1:].shape[0] * test_data[:,:,1:].shape[1], test_data[:,:,1:].shape[2])))
 reshaped_test_target = torch.tensor(test_target[:,:,1:].reshape((test_target[:,:,1:].shape[0] * test_target[:,:,1:].shape[1], test_target[:,:,1:].shape[2])))
-"""
 
 reshaped_test_data = torch.tensor(test_data[:,:,1:DIMENSION+1].reshape(
     (test_data[:,:,1:DIMENSION+1].shape[0] * test_data[:,:,1:DIMENSION+1].shape[1], test_data[:,:,1:DIMENSION+1].shape[2])), requires_grad=True)
-
 reshaped_test_target = torch.tensor(test_target[:,:,1:DIMENSION+1].reshape(
     (test_target[:,:,1:DIMENSION+1].shape[0] * test_target[:,:,1:DIMENSION+1].shape[1], test_target[:,:,1:DIMENSION+1].shape[2])), requires_grad=True)
+"""
 
 # Adding some arguments
 parser = argparse.ArgumentParser(prog="learn_and_test.py",
                                  description="A pytorch code for learning and testing phase space\
                                  trajectory prediciton.")
 
-parser.add_argument("--epochs", default=1000, type=int, help="number of epoches for the model to train")
+parser.add_argument("--epochs", default=200, type=int, help="number of epoches for the model to train")
+parser.add_argument("--batch_size", default=50, type=int, help="batch size for training of the model")
 parser.add_argument("--dt", default=0.02, type=float, help="size of the time step used in the simulation")
 parser.add_argument('--train', default=True, action=argparse.BooleanOptionalAction, help="do you wish to train a new model?")
 parser.add_argument('--plot', default=True, action=argparse.BooleanOptionalAction, help="option of plotting the loss function")
@@ -67,24 +88,41 @@ print(f"Using {DEVICE} for tensor calculations")
 
 # Defining the neural network
 class EntropyNetwork(nn.Module):
+    """
+        For the entropy network we are using a fully input concave neural network achitecture,
+        originally designed by myself - it's a simple alteration of FICNN - fully input convex neural nets,
+        we just need to use concave, decreasing activation functions and negative weights instead of positive ones.
+    """
     def __init__(self):
         super().__init__()
-        self.S = nn.Sequential(
-            nn.Linear(DIMENSION, 20).double(),
-            nn.Softplus(),
-            nn.Linear(20, 20).double(),
-            nn.Softplus(),
-            nn.Linear(20, 20).double(),
-            nn.Softplus(),
-            nn.Linear(20, 1).double()
-        )
+        self.input_layer = nn.Linear(DIMENSION, 20).double()
 
-    def forward(self, x):
-        return self.S(x)
+        self.prop_layer1 = nn.Linear(20, 20).double()
+        self.lateral_layer1 = nn.Linear(DIMENSION, 20).double()
+
+        self.prop_layer2 = nn.Linear(20, 20).double()
+        self.lateral_layer2 = nn.Linear(DIMENSION, 20).double()
+
+        self.output_layer = nn.Linear(20, 1).double()
+        self.lateral_layer_out = nn.Linear(DIMENSION, 1).double()
+
+    def forward(self, x0):
+        x = -nn.Softplus()(self.input_layer(x0))
+
+        x = -nn.Softplus()(self.prop_layer1(x) + self.lateral_layer1(x0))
+        self.prop_layer1.weight.data = -torch.abs(self.prop_layer1.weight.data)
+
+        x = -nn.Softplus()(self.prop_layer2(x) + self.lateral_layer2(x0))
+        self.prop_layer2.weight.data = -torch.abs(self.prop_layer2.weight.data)
+
+        x_final = -nn.Softplus()(self.output_layer(x) + self.lateral_layer_out(x0))
+        self.output_layer.weight.data = -torch.abs(self.output_layer.weight.data)
         
+        return x_final
+
 class DissipationNetwork(nn.Module):
     """
-        For dissipation potential network we are using a more complex architecture to ensure convexity of the output.
+        For the dissipation potential network we are using a more complex architecture to ensure convexity of the output.
         Specifically: PICNN, source: https://arxiv.org/pdf/1609.07152
     """
     def __init__(self):
@@ -103,17 +141,17 @@ class DissipationNetwork(nn.Module):
         # The branch that propagates x_star forward (We need to enforce convexity here)
         self.conjugate_prop_layer_1 = nn.Linear(20, 20, bias=False).double()
         self.conjugate_prop_layer_2 = nn.Linear(20, 20, bias=False).double()
-        self.conjugate_prop_layer_out= nn.Linear(20, 1).double()
+        self.conjugate_prop_layer_out= nn.Linear(20, 1, bias=False).double()
 
-        self.conjugate_prop_layer_1_mid = nn.Linear(20, 20, bias=False).double()
-        self.conjugate_prop_layer_2_mid = nn.Linear(20, 20, bias=False).double()
-        self.conjugate_prop_layer_out_mid = nn.Linear(20, 20, bias=False).double()
+        self.conjugate_prop_layer_1_mid = nn.Linear(20, 20).double()
+        self.conjugate_prop_layer_2_mid = nn.Linear(20, 20).double()
+        self.conjugate_prop_layer_out_mid = nn.Linear(20, 20).double()
 
         # The branch which always starts at x0_star and ends at arbitrary x_star
-        self.conjugate_lateral_layer_in = nn.Linear(DIMENSION, 20).double()
-        self.conjugate_lateral_layer_1 = nn.Linear(DIMENSION, 20).double()
-        self.conjugate_lateral_layer_2 = nn.Linear(DIMENSION, 20).double()
-        self.conjugate_lateral_layer_out = nn.Linear(DIMENSION, 1).double()
+        self.conjugate_lateral_layer_in = nn.Linear(DIMENSION, 20, bias=False).double()
+        self.conjugate_lateral_layer_1 = nn.Linear(DIMENSION, 20, bias=False).double()
+        self.conjugate_lateral_layer_2 = nn.Linear(DIMENSION, 20, bias=False).double()
+        self.conjugate_lateral_layer_out = nn.Linear(DIMENSION, 1, bias=False).double()
 
         self.conjugate_lateral_layer_in_mid = nn.Linear(DIMENSION, DIMENSION).double()
         self.conjugate_lateral_layer_1_mid = nn.Linear(20, DIMENSION).double()
@@ -127,17 +165,17 @@ class DissipationNetwork(nn.Module):
         x_star = nn.Softplus()(self.x_lateral_layer_1(x0) + self.conjugate_lateral_layer_in(torch.mul(x0_star, self.conjugate_lateral_layer_in_mid(x0))))
         x = nn.Softplus()(self.x_input_layer(x0))
 
-        x_star = nn.Softplus()(self.x_lateral_layer_2(x) + self.conjugate_prop_layer_1(torch.mul(x_star, self.conjugate_prop_layer_1_mid(x)))
+        x_star = nn.Softplus()(self.x_lateral_layer_2(x) + self.conjugate_prop_layer_1(torch.mul(x_star, nn.Softplus()(self.conjugate_prop_layer_1_mid(x))))
                                 + self.conjugate_lateral_layer_1(torch.mul(x0_star, self.conjugate_lateral_layer_1_mid(x))))
         x = nn.Softplus()(self.x_prop_layer1(x))
         self.conjugate_prop_layer_1.weight.data = torch.abs(self.conjugate_prop_layer_1.weight.data)
 
-        x_star = nn.Softplus()(self.x_lateral_layer_3(x) + self.conjugate_prop_layer_2(torch.mul(x_star, self.conjugate_prop_layer_2_mid(x)))
+        x_star = nn.Softplus()(self.x_lateral_layer_3(x) + self.conjugate_prop_layer_2(torch.mul(x_star, nn.Softplus()(self.conjugate_prop_layer_2_mid(x))))
                                 + self.conjugate_lateral_layer_2(torch.mul(x0_star, self.conjugate_lateral_layer_2_mid(x))))
         x = nn.Softplus()(self.x_prop_layer2(x))
         self.conjugate_prop_layer_2.weight.data = torch.abs(self.conjugate_prop_layer_2.weight.data)
 
-        out = nn.Softplus()(self.x_lateral_layer_out(x) + self.conjugate_prop_layer_out(torch.mul(x_star, self.conjugate_prop_layer_out_mid(x)))\
+        out = nn.Softplus()(self.x_lateral_layer_out(x) + self.conjugate_prop_layer_out(torch.mul(x_star, nn.Softplus()(self.conjugate_prop_layer_out_mid(x))))\
                                 + self.conjugate_lateral_layer_out(torch.mul(x0_star, self.conjugate_lateral_layer_out_mid(x))))
         self.conjugate_prop_layer_out.weight.data = torch.abs(self.conjugate_prop_layer_out.weight.data)
 
@@ -167,24 +205,24 @@ class GradientDynamics(nn.Module):
 L = nn.MSELoss()
 
 if args.train:
-    reshaped_train_data = torch.tensor(train_data[:,:,1:DIMENSION+1].reshape(
-        (train_data[:,:,1:DIMENSION+1].shape[0] * train_data[:,:,1:DIMENSION+1].shape[1], train_data[:,:,1:DIMENSION+1].shape[2])), requires_grad=True)
-    
-    reshaped_train_target = torch.tensor(train_target[:,:,1:DIMENSION+1].reshape(
-        (train_target[:,:,1:DIMENSION+1].shape[0] * train_target[:,:,1:DIMENSION+1].shape[1], train_target[:,:,1:DIMENSION+1].shape[2])), requires_grad=True)
-        
+    training_trajectories, test_trajectories = random_split(trajectories, [0.8, 0.2])
+    dataloader = DataLoader(dataset=training_trajectories, batch_size=args.batch_size, shuffle=True)
+
     model = GradientDynamics()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
     # Training
     losses = []
-    for k in range(args.epochs):
-        optimizer.zero_grad()
-        loss = L(model(reshaped_train_data)[1]*args.dt + reshaped_train_data, reshaped_train_target)
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
-        print(f"Epoch no. {k}/{args.epochs} done!")
+    for i in range(args.epochs):
+        for j, (trajectory_batch_data, trajectory_batch_target) in enumerate(dataloader):
+            optimizer.zero_grad()
+            predictions = model(trajectory_batch_data)[1]
+            loss = L(predictions * args.dt + trajectory_batch_data, trajectory_batch_target)
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+
+        print(f"Epoch no. {i}/{args.epochs} done!")
 
     if os.path.exists("models"):
         torch.save(model.state_dict(), "models/model.pth")
@@ -196,7 +234,15 @@ else:
     model.load_state_dict(torch.load("models/model.pth", weights_only=True))
     model.eval
 
-MSE_test_set = L(model(reshaped_test_data)[1]*args.dt+reshaped_test_data, reshaped_test_target)
+if args.train:
+    test_data = trajectories.data[test_trajectories.indices]
+    test_target = trajectories.target[test_trajectories.indices]
+
+else:
+    test_data = trajectories.data
+    test_target = trajectories.target
+
+MSE_test_set = L(model(test_data)[1]*args.dt+test_data, test_target)
 print(f"MSE on the test set is: {MSE_test_set}")
 
 if args.plot:
@@ -207,24 +253,25 @@ if args.plot:
         ax1.set_xlabel("Epochs")
         ax1.set_ylabel("MSE")
         ax1.set_title("training loss decline on the training data")
-        ax1.plot(range(len(losses)),losses)
+        ax1.plot(range(len(losses)), losses)
 
-    if torch.numel(reshaped_test_data[0]) == 1:
+    if DIMENSION == 1:
         # Sampling random trajectory and plotting it along with predicted trajectory
         fig2,ax2 = plt.subplots()
-        sample = np.array(test_data[np.random.randint(0,len(test_data)-1)])
+        sample = test_data[np.random.randint(0,len(test_data)-1)].detach().numpy()
+        time = [args.dt*i for i in range(len(sample))]
     
         ax2.set_xlabel("t")
         ax2.set_ylabel("x")
-        ax2.plot(sample[:,0], sample[:,1], label="original data")
+        ax2.plot(time, sample, label="original data")
 
-        prediction = [sample[0,1]]
+        prediction = [sample[0]]
         for i in range(len(sample)):
             prediction.append(float(model(torch.tensor([prediction[i]], requires_grad=True).unsqueeze(0))[1])*args.dt+prediction[i])
 
         prediction = np.array(prediction)
         ax2.set_title(f"MSE of the test set: {MSE_test_set}")
-        ax2.plot(sample[:-2,0], prediction[:-3] , label="prediction")
+        ax2.plot(time[:-2], prediction[:-3] , label="prediction")
         ax2.legend()
 
         # Plotting dissipation potential
